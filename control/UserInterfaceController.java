4b32bd442805ed8c8d5578a470db6ae2abc341c4
Thomastppt <thomastppt@example.com>
2020-11-29 11:15:53 -0700

    added to startPurchase()

--- control/UserInterfaceController.java
+++ control/UserInterfaceController.java
@@ -4,6 +4,8 @@ import model.*;
 import view.*;
 import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
+
 public class UserInterfaceController {
     private UserInterface UI;
     private MovieSearcher movie_searcher;
@@ -64,7 +66,6 @@ public class UserInterfaceController {
 
     public void setShowTime(String showTime) {
         seat_selector.selectShowtime(showTime);
-        ;
     }
 
     public boolean setSeat(ArrayList<String> seat) {
@@ -76,7 +77,29 @@ public class UserInterfaceController {
     }
 
     public void startPurchase() {
-        purchase_process.createReceipt();
+        
+        //checks to see if the user has inputted valid information and selected a seat 
+        if(!ticketInfo.isEmpty() && !userInfo.isEmpty()){
+            purchase_process = new PurchaseProcess(ticketInfo, userInfo);
+            purchase_process.createReceipt();
+        
+            //popup for confirming purchase 
+            int confirmed = JOptionPane.showConfirmDialog(null, "Confirm Purchase?", "User Login", JOptionPane.YES_NO_OPTION);
+
+            //if confirmed, continue
+            if(confirmed == JOptionPane.NO_OPTION){
+                purchase_process.addReciept();
+            }else{
+                //display cancel message 
+                return;
+            }
+            //else return and display 
+
+            purchase_process.emailUser();
+            //print reciept to screen 
+        }else{
+            //pop up informing user to select a seat first 
+        }
     }
 
     public void startRefund(int ticketNum) {
